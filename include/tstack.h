1eea006ea579884cf42daf2bb5fe1eda1cfc2298
unknown <nik.ovsyannikov2090@gmail.com>
2024-12-01 03:02:40 +0300

    done tstack

--- include/tstack.h
+++ include/tstack.h
@@ -1,5 +1,48 @@
 ﻿#pragma once
 #include <iostream>
 #include <stdexcept>
+#include <deque>
+
+template <typename T>
+class Stack
+{
+	std::deque<T> data;
+public:
+	Stack() 
+	{
+	}
+
+	void push(const T& val)
+	{
+		data.push_back(val);
+	}
+
+	void pop()
+	{
+		if (empty()) throw "stack is empty";
+		data.pop_back();
+	}
+
+    T peek() const
+	{
+		if (!empty()) return data.back();
+		throw "stack is empty";
+	}
+
+	size_t get_size() const
+	{
+		return data.size();
+	}
+
+	bool empty() const
+	{
+		return data.empty();
+	}
+
+	void clear()
+	{
+		data.clear();
+	}
+};
 
 
